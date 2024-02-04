ae4161e995f4a1e792f8a4400803fe9140c1b116
Akarsh Rajput <112371948+akarshrajput@users.noreply.github.com>
2024-02-04 21:04:52 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./index.css";
 
-function App() {
+export default function App() {
+  const [inputSearch, setInputSearch] = useState("");
+  const [dataArray, setDataArray] = useState([]);
+
+  const handleInputSearch = function (value) {
+    setInputSearch(value);
+  };
+
+  useEffect(
+    function () {
+      async function fetchData() {
+        try {
+          const res = await fetch(
+            `https://wordbase.up.railway.app/word/${inputSearch}`
+          );
+          const data = await res.json();
+          if (inputSearch === "") {
+            setDataArray([]);
+            return;
+          }
+          setDataArray(data.data.definitions);
+          // console.log(data.data);
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      }
+
+      fetchData();
+    },
+    [inputSearch]
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header inputSearch={inputSearch} onInputSearch={handleInputSearch} />
+      <Body dataArray={dataArray} />
     </div>
   );
 }
 
-export default App;
+function Header({ inputSearch, onInputSearch }) {
+  return (
+    <div className="header">
+      <Logo />
+      <Search inputSearch={inputSearch} onInputSearch={onInputSearch} />
+      <Container />
+    </div>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="logo">
+      <p>Wordbase</p>
+    </div>
+  );
+}
+
+function Search({ inputSearch, onInputSearch }) {
+  return (
+    <div className="search-container">
+      <input
+        type="text"
+        placeholder="Search Word eg. school"
+        value={inputSearch}
+        onChange={(e) => onInputSearch(e.target.value)}
+      />
+    </div>
+  );
+}
+
+function Container() {
+  return <div></div>;
+}
+
+function Body({ dataArray }) {
+  return (
+    <div className="word-list-flex">
+      {dataArray.length > 0 ? (
+        dataArray.map((items) => <WordList key={items.id} items={items} />)
+      ) : (
+        <p></p>
+      )}
+    </div>
+  );
+}
+
+function WordList({ items }) {
+  const data = items.gloss.split(";");
+  const dataSetMain = data[0];
+  const firstLetter = dataSetMain.charAt(0).toUpperCase();
+  const remainingChars = dataSetMain.slice(1);
+  const capitalizedDataSetMain = firstLetter + remainingChars;
+  return (
+    <div className="word-list-item">
+      <p>{items.id}.</p>
+      <div className="word-list-item-container">
+        <p>{capitalizedDataSetMain}</p>
+        <p>Example - {data[1]}</p>
+      </div>
+    </div>
+  );
+}
